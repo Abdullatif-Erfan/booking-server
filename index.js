c5aec065c08d8da271d14232b27a52bbd374000f
Abdul Latif Erfan <60866963+Abdullatif-Erfan@users.noreply.github.com>
2022-11-18 11:46:44 +0430

    cors added into server js file

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const app = express();
 dotenv.config();
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO); // onlien
